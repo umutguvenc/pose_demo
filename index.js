51156540e24df7e1c4862ebc7da1e0790f13a850
umutguvenc <77326034+umutguvenc@users.noreply.github.com>
2022-10-18 22:13:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan('dev'));
